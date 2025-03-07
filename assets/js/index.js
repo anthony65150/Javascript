c660ebf35f8026c0ccfb90d97846198ad78bb1f0
Anthony Rodrigues <anthony-rodrigues31@hotmail.fr>
2025-03-07 21:39:54 +0100

    creation index

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+//commentaire sur une ligne.
+
+/*commentaire sur
+plusieurs
+lignes*/
+
+//Variables
+let age = 25; //variable qui peut etre modifier
+const PI = 3.14; //variable qui ne peut pas etre modifier
+console.log(age);
+
