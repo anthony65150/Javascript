2d5244463de1236ec387595811211908b95a944b
Rodrigues Anthony <anthony-rodrigues31@hotmail.fr>
2025-03-12 21:29:01 +0100

    creation et suppression d'element

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -19,7 +19,7 @@ document.querySelectorAll('p'); // selectionner tout les "p" du html
 // exercice
 
 //1----changer le texte du paragraphe
-let changerParagraphe = document.querySelector('.texte');
+/*let changerParagraphe = document.querySelector('.texte');
 changerParagraphe.textContent = "nouveau texte modifié";
 
 //2-----modifier la valeur de l'input
@@ -36,10 +36,49 @@ editerMoi.appendChild(nouveauParagraphe);
 nouveauParagraphe.style.color = "blue";
 
 
-//manipulation des classes
+//manipulation des classes----------------------------------------------------------------------------------------------
 
 //element.classList.add('classe');      ajouter une classe
 //element.classList.remove('classe');   supprimer une classe
 //element.classList.toggle('classe');   basculer une classe
 //element.classList.contains('classe'); verifier si une classe existe
+*/
+//exercice classes
+
+//1. ajouter un evenement click qui bascule de carre a rond
+//2. verifier la presence d'une classe avant de la modifier
+/*let box = document.getElementById("box");//je recupere mon element box
+box.addEventListener("click", function(){
+    box.style.transition = "all 0.3s ease";// on peut rajouter une transition
+    if(box.classList.contains("carre")){
+        box.classList.toggle("rond");
+    }
+    else{
+        box.classList.toggle("carre");
+    }
+})
+*/
+//plus court
+/*let box = document.getElementById("box");
+
+box.addEventListener("click", function(){
+    box.style.transition = "all 0.4s ease";
+    box.classList.toggle("rond");
+})*/
+
+
+//creation et suppression d'elements
+//document.createElement("div");creer un nouveau element
+//parent.appendChild(element); ajoute un element enfant
+//parent.removeChild(element); supprime un element enfant
+//elemnt.remove(); on supprime un element
+
+
+
+//exercice----------------------------------------------
+//1. creer une fonction qui ajoute un nouvel element a la liste
+//2. ajouter un bouton de suppression a chaque element
+//3. implementer la suppression d'element
+
+
 
